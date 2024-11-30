c9c3422b0ddfc064c89476a43026d626111ace3e
Brennon Allen <brennon.t.allen@gmail.com>
2024-11-30 05:04:49 -0600

    ok

--- src/components/WelcomeBox.jsx
+++ src/components/WelcomeBox.jsx
@@ -18,17 +18,17 @@ export default function WelcomeBox() {
         <h1 className="landing-subtitle">We Specialize In</h1>
         <h1 className="welcome-title">CUSTOM WEBSITE DEVELOPMENT SERVICES</h1>
         <br></br>
-        <div className="flex-center-column button-box">
-          <button onClick={openCalc} className="calculate-button">Calculate Estimate</button>
-          <button className="button-fix">Schedule Consultation</button>
-        </div>
-      </div>
-      <h2 className="landing-subtitle desc-text">
+        <h2 className="desc-text landing-subtitle">
           Use our <span className="bolder">Cost Estimater Tool</span> to get a
           quote instantly, or{" "}
           <span className="bolder">Schedule a Free Consultation</span> with one
           of our developers.{" "}
         </h2>
+        <div className="flex-center-row button-box">
+          <button onClick={openCalc} className="calculate-button">Calculate Estimate</button>
+          <button className="button-fix">Schedule Consultation</button>
+        </div>
+      </div>
     </>
   );
 }
